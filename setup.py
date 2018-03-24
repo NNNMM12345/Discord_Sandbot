680d69fa80ecef34d4feedbac17257e08300907a
Eloi <e.cousinet@gmail.com>
2018-03-24 23:20:08 +0000

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+
+# Learn more: https://github.com/NNNMM12345/Discord_Sandbot
+
+from setuptools import setup, find_packages
+
+with open('README.md') as f:
+    readme = f.read()
+    
+with open('LICENSE') as f:
+    license = f.read()
+
+setup(
+    name='Sandbot',
+    version='0.0.1',
+    description='The Awesome Bot',
+    long_description=readme,
+    author='Who',
+    url='https://github.com/NNNMM12345/Discord_Sandbot',
+    license=license
+)
